dc53348b95ee6df8e6dfd9870b7a47ee27395db5
kdiaziglesias <quixoskdi@gmail.com>
2015-03-11 10:57:05 +0100

    Uso de nuestra base de datos en una Activity

--- app/src/main/java/com/example/kdiaziglesias/sqlite/MainActivity.java
+++ app/src/main/java/com/example/kdiaziglesias/sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kdiaziglesias.sqlite;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,10 +21,34 @@ public class MainActivity extends ActionBarActivity {
         MDB.insertarCONTACTO(3,"Maria",1111111,"Maria@DB.es");
         MDB.insertarCONTACTO(4,"Daniel",1111111,"daniel@DB.es");
 
-        //Log.d("Total",Integer.toString(MDB.));
+        Log.d("Total", Integer.toString(MDB.recuperarCONTACTOS().size()));
+        int[] ids = new int[MDB.recuperarCONTACTOS().size()];
+        String[] noms = new String[MDB.recuperarCONTACTOS().size()];
+        int [] tlfs = new int[MDB.recuperarCONTACTOS().size()];
+        String [] emls = new String[MDB.recuperarCONTACTOS().size()];
+        for (int i = 0; i < MDB.recuperarCONTACTOS().size(); i++) {
+            ids[i] = MDB.recuperarCONTACTOS().get(i).getID();
+            noms[i] = MDB.recuperarCONTACTOS().get(i).getNOMBRE();
+            tlfs[i] = MDB.recuperarCONTACTOS().get(i).getTELEFONO();
+            emls[i] = MDB.recuperarCONTACTOS().get(i).getEMAIL();
+            Log.d(""+ids[i], noms[i] + ", " + tlfs[i] + ", " + emls[i]);
+        }
 
+        // Modificamos el registro 3
+        MDB.modificarCONTACTO(3, "PPPPP", 121212121, "xxxx@xxxx.es");
 
+        // Recuperamos el 3 registro y lo mostramos en el log
+        int id = MDB.recuperarCONTACTO(3).getID();
+        String nombre = MDB.recuperarCONTACTO(3).getNOMBRE();
+        int telefono = MDB.recuperarCONTACTO(3).getTELEFONO();
+        String email = MDB.recuperarCONTACTO(3).getEMAIL();
+        Log.d(""+id, nombre + ", " + telefono + ", " + email);
+
+        // Borramos el registro 3
+        MDB.borrarCONTACTO(3);
     }
+
+
 
 
     @Override
